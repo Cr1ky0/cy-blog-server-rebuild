e8a646b5984326a53552f63d6d9e6672a0d1cb46
Creekyu <xby503094716@gmail.com>
2023-10-30 14:17:03 +0800

    fix

--- src/main/java/com/criiky0/mapper/BlogMapper.java
+++ src/main/java/com/criiky0/mapper/BlogMapper.java
@@ -1,12 +1,7 @@
 package com.criiky0.mapper;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.criiky0.pojo.Blog;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.MapKey;
-
-import java.util.Map;
 
 /**
 * @author criiky0
